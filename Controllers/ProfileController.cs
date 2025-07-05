67616e9de203d68f890ccd78a951dd01faddc8e1
MdSrabon <mdsrabon.cse@gmail.com>
2025-07-05 16:47:25 +0600

    update

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProfileIndex()
+        {
+            return View();
+        }
+    }
+}
